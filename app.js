bcd4dca94a3956d6af993dff7b90226277963738
Difelice-e <difelice.emanuele@gmail.com>
2022-02-25 14:28:51 +0100

    creato layout

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 let app = new Vue(
     {
-        el: '#root',
-        data: {}, 
+        el: '#container-vue',
+        data: {
+            toDoList: [
+                {
+                    text: 'Fare la Spesa',
+                    done: false
+                },
+                {
+                    text: 'Completare la todolist',
+                    done: false
+                },
+                {
+                    text: 'Dare da mangiare al cane',
+                    done: false
+                },
+                {
+                    text: 'Rifare il letto',
+                    done: false
+                },
+            ],
+        }, 
         methods: {}
     }
 )
